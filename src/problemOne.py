54463b0bead038394791d7c6177a566633e39523
Barisa Obradovic <bbaja42@gmail.com>
2012-05-27 12:02:39 +0200

    Improved problem 1, solved 2

--- src/problemOne.py
+++ src/problemOne.py
@@ -6,5 +6,13 @@ The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
 
-result = sum([x for x in range(1000) if x % 3 == 0 or x % 5 == 0])
-print (result)
+
+def find_sum():
+    result = sum([x for x in range(1000) if x % 3 == 0 or x % 5 == 0])
+    return result
+
+print ("Sum is {}".format(find_sum()))
+
+import timeit
+t = timeit.Timer("find_sum()", "from __main__ import find_sum")
+print ("Average running time: {} seconds".format(t.timeit(1000)))
